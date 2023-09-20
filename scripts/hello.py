24c5a39f57c42f9d22f6451f7081a945d523744d
Henrique Alberti <alberti.rick@gmail.com>
2023-09-19 21:53:55 -0300

    COMMIT COMMIT COMMIT TRANSLATION

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
+from gettext import gettext as _
 from dev_aberto import hello
+# Initialize the gettext system
+import gettext
+gettext.bindtextdomain('cli', 'locale')
+gettext.textdomain('cli')
+_ = gettext.gettext
 
+# cli é o nome do arquivo em que guardamos nossas traduções
+# localedir é o caminho onde estão armazenadas as traduções. Pode ser um caminho relativo. 
 if __name__ == '__main__':
     date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    print(_('Último commit feito em:'), date, _(' por'), name)
